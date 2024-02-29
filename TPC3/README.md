1bee847a49ea187bf895a2ebafe3a1d71e26add1
Ricardo Araujo <ricarlojo@gmail.com>
2024-02-29 19:24:53 +0000

    TPC3 - README.md; update extract.py;html e css

---
+++ TPC3/README.md
@@ -0,0 +1,11 @@
+## Ficheiro extract.py
+
+Este TPC consiste no algoritmo desenvolvido na aula, tendo sido acrescentada uma substituição através da expressão regex "\n\f" por "\n\n", antes de retirar "\f", de modo a lidar com as situações em que devido à quebra de página, a palavra-chave não tinha quebra de linha.
+
+### Ficheiro HTML
+
+Foi acrescentada a boilerplate clássica de HTML ao ficheiro criado no processo, um elemento div de forma a agrupar a palavra-chave e a descrição.
+
+Foi também acrescentado um elemento div de header, contendo o título e descrição do trabalho.
+
+Por último foi criado um ficheiro css para tornar a página criada mais agradável.
---
+++ TPC3/extract.py
@@ -0,0 +1,70 @@
+import re
+
+doc_text_path ="dicionario_medico.txt"
+
+doc_t = open(doc_text_path,'r', encoding="utf-8")
+
+doc = doc_t.read()
+
+# data cleaning
+
+doc = re.sub(r"\n\f","\n\n", doc)  # colocar uma linha em branco nas quebras de pagina
+doc = re.sub(r"\f","", doc)
+
+
+# marcar designações
+
+print(doc)
+
+doc = re.sub(r"\n\n(.+)", r"\n\n@\1", doc)
+
+doc = re.sub(r"@(.+)\n\n@", r"@\1\n", doc)
+
+#extrair termos
+termos = re.findall(r"@(.+)\n",doc)
+
+conj = re.findall(r"@(.+)\n([^@]+)",doc)   # daqui sai um tuplo
+
+#print(termos[0:50])
+#print(conj)
+
+
+# uma alternativa seria 
+
+# extrair termos
+
+conceitos = re.split(r"\n{2,}",doc)
+
+#termos = [tuple(conceito.split("\n",maxsplit=1)) for conceito in conceitos]
+
+#print(termos)
+
+
+# formar o HTML
+body =""
+
+titulo = "<h2> Dicionário Médico </h2>"
+descricao = '<p class="title"> Este é um dicionário médico desenvolvido na disciplina de SPLEB </p>'
+
+for termo in conj:
+    body+='<div class="elemento">'
+    body+= f"<h5>{termo[0]} </h5>"
+    body += f"<p> {termo[1]} </p>"
+    body+="</div>"
+body+= "</body></html>"
+
+html ="""<!DOCTYPE html>
+    <html lang="pt">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="convertido.css" />
+        <title>TPC3</title>
+    </head>
+    <body>
+    <div class="header">""" +titulo + descricao+'</div>' + body
+
+file_out = open("aula3.html","w")
+file_out.write(html)
+file_out.close()
+
